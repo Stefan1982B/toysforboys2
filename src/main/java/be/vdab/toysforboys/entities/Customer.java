145a0ffae11a287f6dcccde01970f48f6e237394
programmingMadeEasy1 <programming_made_easy@outlook.be>
2018-08-18 13:19:03 +0200

    tem orders

--- src/main/java/be/vdab/toysforboys/entities/Customer.java
+++ src/main/java/be/vdab/toysforboys/entities/Customer.java
@@ -84,7 +84,7 @@ public class Customer implements Serializable {
 		if (order == null) {
 			throw new NullPointerException();
 		}
-		boolean toegevoegd = orders.remove(order);
+		boolean toegevoegd = orders.add(order);
 		Customer oudeCustomer = order.getCustomer();
 		if(oudeCustomer != null && oudeCustomer != this) {
 			oudeCustomer.orders.remove(order);
@@ -93,7 +93,7 @@ public class Customer implements Serializable {
 			order.setCustomer(this);
 		}
 		
-		return orders.add(order);
+		return toegevoegd;
 	}
 	
 	public boolean removeOrder(Order order) {
--- src/test/java/be/vdab/toysforboys/entities/CustomerTest.java
+++ src/test/java/be/vdab/toysforboys/entities/CustomerTest.java
@@ -4,10 +4,12 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
 
 import org.junit.Before;
 import org.junit.Test;
 
+import be.vdab.toysforboys.enums.Status;
 import be.vdab.toysforboys.valueobjects.Address;
 
 public class CustomerTest {
@@ -20,22 +22,22 @@ public class CustomerTest {
 	public void before() {
 		customer1 = new Customer("testCustomer1", 1, new Address("testStraat1", "testCity1", "testState1", "testPostalCode1"), new Country("testCountry1", 1) );
 		customer2 = new Customer("testCustomer2", 1, new Address("testStraat2", "testCity2", "testState2", "testPostalCode2"), new Country("testCountry2", 1) );
-		order1 = new Order(LocalDate.of(2019,1,1), LocalDate.of(2019,2,2), LocalDate.of(2019,1,15));
+		order1 = new Order(LocalDate.of(2019,1,1), LocalDate.of(2019,2,2), LocalDate.of(2019,1,15), "testComment", customer1, Status.SHIPPED, 1);
 	}
 
 	@Test
-	public void campus1IsDeCampusVanDocent1() {
-		assertEquals(campus1, docent1.getCampus());
-		assertEquals(1, campus1.getDocenten().size());
-		assertTrue(campus1.getDocenten().contains(docent1));
+	public void order1IsVanCustomer1() {
+		assertEquals(customer1, order1.getCustomer());
+		assertEquals(1, customer1.getOrders().size());
+		assertTrue(customer1.getOrders().contains(order1));
 	}
 
 	@Test
-	public void docent1VerhuistNaarCampus2() {
-		assertTrue(campus2.add(docent1));
-		assertTrue(campus1.getDocenten().isEmpty());
-		assertEquals(1, campus2.getDocenten().size());
-		assertTrue(campus2.getDocenten().contains(docent1));
-		assertEquals(campus2, docent1.getCampus());
+	public void order1WordtVerplaatstNaarCustomer2() {
+		assertTrue(customer2.addOrder(order1));
+		assertTrue(customer1.getOrders().isEmpty());
+		assertEquals(1, customer2.getOrders().size());
+		assertTrue(customer2.getOrders().contains(order1));
+		assertEquals(customer2, order1.getCustomer());
 	}
 }
